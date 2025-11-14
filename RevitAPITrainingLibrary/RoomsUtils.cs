a816bce1b0b5cc007c0100a126681404c424b7ec
alex206 <alexeev108@yandex.ru>
2025-11-14 20:58:54 +0300

    feat: добавлены методы в классы

--- RevitAPITrainingLibrary/RoomsUtils.cs
+++ RevitAPITrainingLibrary/RoomsUtils.cs
@@ -1,9 +1,10 @@
-﻿using Autodesk.Revit.Creation;
-using Autodesk.Revit.DB;
+﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Mechanical;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,22 @@ using System.Threading.Tasks;
 namespace RevitAPITrainingLibrary
 {
     public class RoomsUtils
-    {        
+    {
+        public static List<Room> GetAllRooms(ExternalCommandData commandData)
+        {
+            UIApplication uiApplication = commandData.Application;
+            UIDocument uIDocument = uiApplication.ActiveUIDocument;
+            Document document = uIDocument.Document;
+
+            List<Room> allRooms = new FilteredElementCollector(document)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .WhereElementIsNotElementType()
+                .Cast<Room>()
+                .ToList();                
+
+            return allRooms;
+        }
+
         public static Room GetRoomByPoint(Autodesk.Revit.DB.Document document, XYZ point)
         {
             FilteredElementCollector collector = new FilteredElementCollector(document);
@@ -27,5 +43,7 @@ namespace RevitAPITrainingLibrary
             }
             return null;
         }
+
+
     }
 }
